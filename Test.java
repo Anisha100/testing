d03b8a71cb1b067dcc9ab501e2d3cb5ec839d86c
Anisha Ghosh <anishagh28@gmail.com>
2023-09-07 17:32:32 +0530

    New Repository

---
+++ Test.java
@@ -0,0 +1,50 @@
+
+import org.json.*;
+import java.util.*;
+import java.security.*;
+import java.io.*;
+public class Test
+{
+    public static void main(String[] args) throws Exception 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Is the device on(yes/no)?: ");
+        String dev_st=sc.nextLine();
+        boolean dev=dev_st.toLowerCase().contains("yes");
+        System.out.println("Is the encryption on(yes/no)?: ");
+        String ecr_st=sc.nextLine();
+        boolean ecr=ecr_st.toLowerCase().contains("yes");
+        String key= "";
+        if(ecr){
+            System.out.println("Generating a new encryption key .....");
+            //String key=sc.nextLine();
+            byte[] bytes = new byte[16];
+            SecureRandom.getInstanceStrong().nextBytes(bytes);
+            /* String hex="";
+            for(byte i:bytes)
+            hex+=String.format("%02X",i);
+            System.out.print(hex);
+            System.out.println();
+             */
+            byte[] encoded = Base64.getEncoder().encode(bytes);
+            key=new String(encoded);
+
+        }
+        JSONObject jsonobj=new JSONObject();
+        jsonobj.put("device_status",dev);
+        jsonobj.put("encryption_status",ecr);
+        jsonobj.put("key",key);
+        String fln=jsonobj.toString();
+        System.out.println(fln);
+        try{
+            BufferedWriter bw=new BufferedWriter(new FileWriter("objects.json"));
+            bw.write(fln);
+            bw.close();
+        }
+        catch (Exception E)
+        {
+
+        }
+    }
+
+}
